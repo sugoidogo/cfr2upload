67d8d3651fc51a05df2ed1d479eab00715136c19
sugoidogo <sugoidogo@sugoidogo.com>
2026-08-14 16:45:51 -0700

    multipart upload, add wrangler types

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,4 @@
-async function authenticate(request: Request, env: any) {
+async function authenticate(request: Request, env: Env) {
     const authHeader = request.headers.get("Authorization");
     if (!authHeader || !authHeader.startsWith("Basic ")) {
         return false;
@@ -16,7 +16,7 @@ async function authenticate(request: Request, env: any) {
 }
 
 export default {
-    async fetch(request: Request, env: any) {
+    async fetch(request: Request, env: Env) {
         const url = new URL(request.url);
         const bucket = env.BUCKET;
         const key = url.pathname.slice(1);
@@ -44,9 +44,6 @@ export default {
                         },
                     });
                 }
-            }
-            return fetch(request);
-        }
             }
             
             if (request.method === "GET") {
@@ -108,7 +105,7 @@ export default {
                     }
 
                     const multipartUpload = bucket.resumeMultipartUpload(key, uploadId);
-                    const body = await request.json();
+                    const body:any = await request.json();
                     if (!body || !body.parts) {
                         return new Response("Missing or incomplete body", { status: 400 });
                     }
